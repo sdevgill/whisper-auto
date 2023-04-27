479739b08e446a1d951f8735f5e3dfaf1ce44562
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-04-27 20:27:21 +0100

    Add print line for better readability

--- app.py
+++ app.py
@@ -39,6 +39,8 @@ def main():
     transcription_count = 0
     total_time = 0
 
+    print("--------------------------------")  # Add horizontal line at the start
+
     # Iterate through all files in the input folder
     for filename in os.listdir(input_folder):
         # Check if the file has a valid audio file extension
@@ -60,7 +62,7 @@ def main():
 
             # Append the time taken to the output file
             with open(output_file_path, "a", encoding="utf-8") as f:
-                f.write(f"\nTime taken for this transcription: {formatted_time}\n")
+                f.write(f"\nTime: {formatted_time}\n")
 
             transcription_count += 1
             print(
@@ -74,6 +76,8 @@ def main():
 
     print(f"\nFinished processing {transcription_count} audio files.\n")
     print(f"Total time: {total_time_formatted}")
+
+    print("--------------------------------")  # Add horizontal line at the end
 
 
 if __name__ == "__main__":
